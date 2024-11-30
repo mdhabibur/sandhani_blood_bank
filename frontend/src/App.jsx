5f3cba9e4350b411e6a4f189c50575ffde049846
mdhabibur <mdhrahman532@gmail.com>
2024-11-30 16:55:25 +0600

    tailwind set up completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <p className="read-the-docs">
+      <p className="text-4xl font-bold">
         Click on the Vite and React logos to learn more
       </p>
     </>
