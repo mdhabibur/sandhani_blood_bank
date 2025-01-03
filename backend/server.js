129bb91472990d691060e2125e626505cd1702a8
mdhabibur <mdhrahman532@gmail.com>
2025-01-03 19:04:06 +0600

    mongoDB connection setup completed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,18 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import connectDB from './db/connectDB.js'
+
+dotenv.config()
 
 const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    credentials: true,
+}))
 
 const PORT = process.env.PORT || 5000
 
@@ -10,5 +22,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
+    connectDB()
     console.log(`Server is running on port ${PORT}`);
 });
