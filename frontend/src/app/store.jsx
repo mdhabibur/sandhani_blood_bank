1db6606506e49eb6a9223c437361ae5b98a72a6f
mdhabibur <mdhrahman532@gmail.com>
2025-01-07 21:38:20 +0600

    redux persist configured for permanent auth state management

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -1,10 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../redux/auth/authSlice.jsx";
+import storage from "redux-persist/lib/storage"; // Default: localStorage for web
+import { persistReducer, persistStore } from "redux-persist";
+
+
+// Persist configuration for authReducer
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+//wrap authReducer with persistReducer
+const persistedAuthReducer = persistReducer(persistConfig, authReducer)
+
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedAuthReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false
+  })
 });
 
-export default store;
+//Persistor for store
+const persistor = persistStore(store)
+
+export { store, persistor };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from "./app/store.jsx";
+import { store, persistor } from "./app/store.jsx";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Provider store={store}>
-			<App />
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</StrictMode>
 );
